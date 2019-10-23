010bdacb8cca1f5a1b7a9d1f8add00992b39b276
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-10-23 15:12:45 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,6 +7,6 @@ node
   }
   stage("Deploy code to tomcat server")
   {
-    sh 'sudo cp target/*.war /usr/lib/tomcat/webapps/'
+    sh 'sudo cp sm-shop/target/*.war /usr/lib/tomcat/webapps/'
   }
 }
