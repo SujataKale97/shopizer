2359ddbefd8def3954971e94b3b041fc85bc68a0
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-10-23 15:03:29 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,13 @@
+node
+{
+  stage("Build Code")
+  {
+     git 'https://github.com/SujataKale97/shopizer.git'
+    sh 'sudo mvn install'
+  }
+  stage("Deploy code to tomcat server")
+  {
+    sh 'sudo cp target/*.war /usr/lib/tomcat/webapps/'
+  }
+}
+© 2019 GitHub, I
